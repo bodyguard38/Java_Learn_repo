6f3540d5e287d90cf20f2ae5bd469fceb9992d32
ahmet aktas <ahmetmunip38@gmail.com>
2023-02-28 21:56:53 +0100

    last

--- src/j03_ScannerClass_TypeCasting/tasks/Task03.java
+++ src/j03_ScannerClass_TypeCasting/tasks/Task03.java
@@ -1,5 +1,7 @@
 package j03_ScannerClass_TypeCasting.tasks;
 
+import java.util.Scanner;
+
 public class Task03 {
     public static void main(String[] args) {
         /*
@@ -11,5 +13,16 @@ public class Task03 {
          * Ornek : Input : 8
          *         Output : Ayda 10, yilda 121, 40 yilda 4866 gununuz uykuda geciyor
          */
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many hours do u sleep a day?");
+        int sleepTime = scanner.nextInt();
+
+        int month=sleepTime*30;
+        int year=sleepTime*365;
+        int fortyYear=sleepTime*40*365;
+
+        System.out.println("month = " + month);
+        System.out.println("year = " + year);
+        System.out.println("fortyYear = " + fortyYear);
     }
 }
