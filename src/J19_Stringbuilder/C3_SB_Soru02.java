1b84927a18d78ed45eb8774f0d45ccacb812a795
ahmet aktas <ahmetmunip38@gmail.com>
2023-03-01 23:13:09 +0100

    stringBulider

---
+++ src/J19_Stringbuilder/C3_SB_Soru02.java
@@ -0,0 +1,15 @@
+package J19_Stringbuilder;
+
+public class C3_SB_Soru02 {
+    public static void main(String[] args) {
+
+        String s1 ="Java";
+        StringBuilder s2=new StringBuilder("Java");
+        //   if (s1==s2) {    // CTE
+        System.out.println("1");
+        //    } else {
+        System.out.println("2");
+    }
+
+
+}
