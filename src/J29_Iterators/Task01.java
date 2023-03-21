76c96cff5abc04da099cdd5bb084b33d085ba9b2
ahmet aktas <ahmetmunip38@gmail.com>
2023-03-21 21:38:04 +0100

    iterator

---
+++ src/J29_Iterators/Task01.java
@@ -0,0 +1,9 @@
+package J29_Iterators;
+
+public class Task01 {
+      /*
+      task -> Verilen bir integer listin tek  elemanrının karesini tersten iteartor  print eden code create ediniz
+       */
+
+
+}
--- src/s.java
+++ src/s.java
@@ -1,8 +1,19 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 public class s {
     public static void main(String[] args) {
 
+        List<String> l1=new ArrayList<>(Arrays.asList("Harika eda", "Ceren", "Tugba","taha","Ismail","Murat"));
+
+        Iterator<String> it1=l1.iterator();
+
+        while (it1.hasNext()) {
+            System.out.print(it1.next()+" ");
+
+        }
 
     }
 }
